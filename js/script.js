465849dc2237af521492fc36b8efe96d017bdd1e
varunvaruns9 <varunjustrocks@gmail.com>
2018-01-23 22:19:39 +0530

    Add typing animation

--- js/script.js
+++ js/script.js
@@ -1,1 +1,34 @@
-// JavaScript 
+// JavaScript
+var motto = "Eat. Code. Sleep. Repeat.";
+var mottoLength = 0;
+var mottoPrint;
+
+$(document).ready(function() {
+  setInterval("cursorAnimation()", 600);
+  mottoPrint = $("#motto");
+  setInterval("typeNerase()", 175);
+});
+
+function cursorAnimation() {
+  $("#cursor").animate({
+    opacity: 0
+  }, "fast", "swing").animate({
+    opacity: 1
+  }, "fast", "swing");
+}
+
+function erase() {
+  mottoPrint.html(motto.substr(0, mottoLength--));
+  if (mottoLength >= 0) {
+    setTimeout("erase()", 20);
+  } else {
+    mottoLength = 0;
+  }
+}
+
+function typeNerase() {
+  mottoPrint.html(motto.substr(0, mottoLength++));
+  if (mottoLength >= motto.length + 1) {
+    erase();
+  }
+}
