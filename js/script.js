84d89d2e72de3e066ce646f06d56d3266cbbbb3b
rishi-s8 <33762894+rishi-s8@users.noreply.github.com>
2018-06-21 11:03:17 +0530

    Added Smooth Scrolling for all Links

--- js/script.js
+++ js/script.js
@@ -7,6 +7,18 @@ $(document).ready(function() {
   setInterval("cursorAnimation()", 600);
   mottoPrint = $("#motto");
   setInterval("typeNerase()", 175);
+  $('a[href*="#"]:not([href="#"])').click(function() {
+   if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+     var target = $(this.hash);
+     target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+     if (target.length) {
+       $('html, body').animate({
+         scrollTop: target.offset().top
+       }, 1000);
+       return false;
+     }
+   }
+ });
 });
 
 function cursorAnimation() {
