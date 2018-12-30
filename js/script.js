b3cc6e38f3b2e28e5322ff151986fb26acd07754
rishi-s8 <33762894+rishi-s8@users.noreply.github.com>
2018-12-30 16:17:03 +0530

    Add function fillContributors() in js/scripts.js to fetch and display the contributors - Change title from WebDevs to Contributors - Fetch Contributors, Name, Avatar and github-login name using Github API - Display Top-9 contributors sorted by total commits

--- js/script.js
+++ js/script.js
@@ -19,7 +19,50 @@ $(document).ready(function() {
      }
    }
  });
+ fillContributors();
 });
+
+function fillContributors()
+{
+  $.ajaxSetup({
+    async: false
+  });
+  $.getJSON('https://api.github.com/repos/kamandprompt/kamandprompt.github.io/stats/contributors',
+  function(data) {
+      data.sort();
+      data.reverse();
+      var contributorsDiv = document.getElementById("contributors");
+      var rows, iter = 0;
+      for(rows = 0; rows<3; ++rows)
+      {
+        contributorsDiv.innerHTML += "<div class=\"row team-box\">";
+        for(i = 0; i<3; ++iter, ++i)
+        {
+          var login = data[iter].author.login, commits = data[iter].total;
+          $.getJSON('https://api.github.com/users/'+login, function(user) {
+            name = user.name;
+            avatar = user.avatar_url;
+            if(name=="null")
+            {
+              name=login;
+            }
+            var newChild = "\
+            <div class=\"col-lg-3 col-sm-4 text-center member\">\
+              <img class=\"img-circle img-responsive img-center team-img\" src=\" " + avatar + "\" alt=\"\">\
+              <h3>" + name + "</h3>\
+              <h4>Commits: " + commits + "</h3>\
+              <div>\
+                <a target=\"_blank\" href=\"https://github.com/" + login + " \"><i class=\"fab fa-github social-button\"></i></a>\
+              </div>\
+            </div>\
+            ";
+            contributorsDiv.innerHTML += newChild;
+          });
+        }
+        contributorsDiv.innerHTML += "</div>";
+      }
+  });
+}
 
 function cursorAnimation() {
   $("#cursor").animate({
