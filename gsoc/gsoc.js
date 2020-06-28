1fc7545831189a390a9c4cef491f0f8ba6255cfe
pranshukharkwal <awesomepranshu@gmail.com>
2020-06-28 16:34:37 +0530

    Change variable names and add comments

--- gsoc/gsoc.js
+++ gsoc/gsoc.js
@@ -1,12 +1,13 @@
 var data = YAML.load('./data.yaml');
-var divtag = document.getElementById("team");
+var divtag = document.getElementById("team"); //Main div tag inside the html
+
 for (year in data) {
     var id = year.split(" ")[3]
     if (!id) {
         id = "Others"
     }
 
-    var container = document.createElement("div");
+    var container = document.createElement("div"); //Create div tag for each year
     container.className = "container-fluid yearly";
     container.id = id
     container.name = id
@@ -26,45 +27,47 @@ for (year in data) {
     container.appendChild(br.cloneNode());
 
     var names = Object.keys(data[year]);
-    var c = 0;
+    var count = 0;  // To iterate through the "names" list
 
     var length = names.length;
     var q = Math.floor(length / 4);
-    var r = length % 4;
-    var c1 = 0,
-        c2 = 0,
-        c3 = 0,
-        c4 = 0;
+    var r = length % 4; //Calculating quotient and remainder to determine the number of rows and number of elements in each row
+
+    var one_element_rows = 0,  //Number of rows with 1 element
+        two_elements_rows = 0,  //Number of rows with 2 elements
+        three_elements_rows = 0,  //Number of rows with 3 elements
+        four_elements_rows = 0;  //Number of rows with 4 elements
+
     if (q == 0) {
         if (r == 1) {
-            c1 = 1;
+            one_element_rows = 1;
         }
         if (r == 2) {
-            c2 = 1;
+            two_elements_rows = 1;
         }
         if (r == 3) {
-            c3 = 1;
+            three_elements_rows = 1;
         }
     } else if (r == 0) {
-        c4 = q;
+        four_elements_rows = q;
     } else if (r == 1) {
-        c4 = q - 1;
-        c3 = 1;
-        c2 = 1;
+        four_elements_rows = q - 1;
+        three_elements_rows = 1;
+        two_elements_rows = 1;
     } else if (r == 2) {
-        c4 = q - 1;
-        c3 = 2;
+        four_elements_rows = q - 1;
+        three_elements_rows = 2;
     } else if (r == 3) {
-        c4 = q;
-        c3 = 1;
+        four_elements_rows = q;
+        three_elements_rows = 1;
     }
 
-    for (var i = 0; i < c4; i++) {
-        var row = document.createElement("div");
+    for (var i = 0; i < four_elements_rows; i++) {  // Creating rows which would contain four elements
+        var row = document.createElement("div"); //Creating div tag for each row
         row.className = "white centered row";
 
         for (var j = 0; j < 4; j++) {
-            var person = document.createElement("div");
+            var person = document.createElement("div"); //Creating div tag for each person
             person.className = "col-lg-3 centered";
 
             var img = document.createElement("img");
@@ -74,19 +77,19 @@ for (year in data) {
             img.width = 120;
             img.alt = data[year][names[c]]['alt'];
 
-            var h4 = document.createElement("h4");
-            var b = document.createElement("b");
-            var pname = document.createTextNode(names[c])
+            var h4 = document.createElement("h4"); //H4 tag
+            var b = document.createElement("b"); //Bold tag
+            var pname = document.createTextNode(names[c]) //Name of the person
             h4.appendChild(b);
             h4.appendChild(pname);
 
-            var afb = document.createElement("a");
-            afb.href = data[year][names[c]]['fb'];
-            afb.className = "fab fa-facebook-f social-button";
+            var facebook-link = document.createElement("a");
+            facebook-link.href = data[year][names[c]]['fb'];
+            facebook-link.className = "fab fa-facebook-f social-button";
 
-            var agh = document.createElement("a");
-            agh.href = data[year][names[c]]['gh'];
-            agh.className = "social-button fab fa-github";
+            var github-link = document.createElement("a");
+            github-link.href = data[year][names[c]]['gh'];
+            github-link.className = "social-button fab fa-github";
 
             var p = document.createElement("p");
             var ptext = document.createTextNode(data[year][names[c]]['org']);
@@ -95,8 +98,8 @@ for (year in data) {
             person.appendChild(img);
             person.appendChild(br);
             person.appendChild(h4);
-            person.appendChild(afb);
-            person.appendChild(agh);
+            person.appendChild(facebook-link);
+            person.appendChild(github-link);
             person.appendChild(p);
 
             c += 1;
@@ -106,12 +109,12 @@ for (year in data) {
     }
 
 
-    for (var i = 0; i < c3; i++) {
+    for (var i = 0; i < three_elements_rows; i++) { // Creating rows which would contain three elements
         var row = document.createElement("div");
         row.className = "white centered row";
 
         for (var j = 0; j < 3; j++) {
-            var person = document.createElement("div");
+            var person = document.createElement("div"); //Creating div tag for each person
             person.className = "col-lg-4 centered";
 
             var img = document.createElement("img");
@@ -127,13 +130,13 @@ for (year in data) {
             h4.appendChild(b);
             h4.appendChild(pname);
 
-            var afb = document.createElement("a");
-            afb.href = data[year][names[c]]['fb'];
-            afb.className = "fab fa-facebook-f social-button";
+            var facebook-link = document.createElement("a");
+            facebook-link.href = data[year][names[c]]['fb'];
+            facebook-link.className = "fab fa-facebook-f social-button";
 
-            var agh = document.createElement("a");
-            agh.href = data[year][names[c]]['gh'];
-            agh.className = "social-button fab fa-github";
+            var github-link = document.createElement("a");
+            github-link.href = data[year][names[c]]['gh'];
+            github-link.className = "social-button fab fa-github";
 
             var p = document.createElement("p");
             var ptext = document.createTextNode(data[year][names[c]]['org']);
@@ -142,17 +145,18 @@ for (year in data) {
             person.appendChild(img);
             person.appendChild(br);
             person.appendChild(h4);
-            person.appendChild(afb);
-            person.appendChild(agh);
+            person.appendChild(facebook-link);
+            person.appendChild(github-link);
             person.appendChild(p);
 
-            c += 1;
+            count += 1;
             row.appendChild(person);
         }
         container.appendChild(row);
     }
 
-    for (var i = 0; i < c2; i++) {
+    for (var i = 0; i < two_elements_rows; i++) { // Creating rows which would contain two elements
+      // For adding two elements, using class "col-lg-3" which would contain 4 elements, out of which first and fourth are empty, and second and third contains the details
         var row = document.createElement("div");
         row.className = "white centered row";
 
@@ -161,7 +165,7 @@ for (year in data) {
         row.appendChild(person);
 
         for (var j = 0; j < 2; j++) {
-            var person = document.createElement("div");
+            var person = document.createElement("div"); //Creating div tag for each person
             person.className = "col-lg-3 centered";
 
             var img = document.createElement("img");
@@ -177,13 +181,13 @@ for (year in data) {
             h4.appendChild(b);
             h4.appendChild(pname);
 
-            var afb = document.createElement("a");
-            afb.href = data[year][names[c]]['fb'];
-            afb.className = "fab fa-facebook-f social-button";
+            var facebook-link = document.createElement("a");
+            facebook-link.href = data[year][names[c]]['fb'];
+            facebook-link.className = "fab fa-facebook-f social-button";
 
-            var agh = document.createElement("a");
-            agh.href = data[year][names[c]]['gh'];
-            agh.className = "social-button fab fa-github";
+            var github-link = document.createElement("a");
+            github-link.href = data[year][names[c]]['gh'];
+            github-link.className = "social-button fab fa-github";
 
             var p = document.createElement("p");
             var ptext = document.createTextNode(data[year][names[c]]['org']);
@@ -192,8 +196,8 @@ for (year in data) {
             person.appendChild(img);
             person.appendChild(br);
             person.appendChild(h4);
-            person.appendChild(afb);
-            person.appendChild(agh);
+            person.appendChild(facebook-link);
+            person.appendChild(github-link);
             person.appendChild(p);
 
             c += 1;
@@ -208,11 +212,13 @@ for (year in data) {
     }
 
 
-    for (var i = 0; i < c1; i++) {
+    for (var i = 0; i < one_element_rows; i++) { // Creating rows which would contain one element
+
+      // For adding one element, using class "col-lg-4" which would contain 3 elements, out of which first and third are empty, and second contains the details
         var row = document.createElement("div");
         row.className = "white centered row";
 
-        var person = document.createElement("div");
+        var person = document.createElement("div"); //Creating div tag for each person
         person.className = "col-lg-4 centered";
         row.appendChild(person);
 
@@ -233,13 +239,13 @@ for (year in data) {
             h4.appendChild(b);
             h4.appendChild(pname);
 
-            var afb = document.createElement("a");
-            afb.href = data[year][names[c]]['fb'];
-            afb.className = "fab fa-facebook-f social-button";
+            var facebook-link = document.createElement("a");
+            facebook-link.href = data[year][names[c]]['fb'];
+            facebook-link.className = "fab fa-facebook-f social-button";
 
-            var agh = document.createElement("a");
-            agh.href = data[year][names[c]]['gh'];
-            agh.className = "social-button fab fa-github";
+            var github-link = document.createElement("a");
+            github-link.href = data[year][names[c]]['gh'];
+            github-link.className = "social-button fab fa-github";
 
             var p = document.createElement("p");
             var ptext = document.createTextNode(data[year][names[c]]['org']);
@@ -248,8 +254,8 @@ for (year in data) {
             person.appendChild(img);
             person.appendChild(br);
             person.appendChild(h4);
-            person.appendChild(afb);
-            person.appendChild(agh);
+            person.appendChild(facebook-link);
+            person.appendChild(github-link);
             person.appendChild(p);
 
             c += 1;
